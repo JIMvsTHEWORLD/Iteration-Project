586d1c657ce1a1b4400631a072261c029aceac3b
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-06-30 12:02:55 -0400

    pulling from upstream

---
+++ __tests__/supertest.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const server = 'http://localhost:3000';
+
+// eslint-disable-next-line no-undef
+
+describe('Route integration', () => {
+	describe('/', () => {
+		describe('GET', () => {
+			it('responds with 200 status and text/html content type', () => {
+				return request(server).get('/').expect('Content-Type', /text\/html/).expect(200);
+			});
+		});
+	});
+});
